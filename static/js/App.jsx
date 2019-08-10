369d72d75af86ee05a4ad794a40b93637484726b
soukaryag <sg4fz@virginia.edu>
2019-08-10 18:21:47 -0400

    working up react+python

--- static/js/App.jsx
+++ static/js/App.jsx
@@ -1,7 +1,29 @@
-// App.jsx
 import React from "react";
+import Hello from "./Hello";
+import { PageHeader } from "react-bootstrap";
+
+require('../css/fullstack.css');
+var $ = require('jquery');
+
+import HeaderBackgroundImage from '../images/header.jpg';
+
 export default class App extends React.Component {
-  render () {
-    return <p> Hello React!</p>;
-  }
+    constructor(props) {
+        super(props);
+    }
+    addHeaderImg() {
+        let headerBg = new Image();
+        headerBg.src = HeaderBackgroundImage;
+    }
+
+    render () {
+        return (
+            <PageHeader>
+                <div className='header-contents'>
+                    {this.addHeaderImg()}
+                    <Hello name='Rimini' />
+                </div>
+            </PageHeader>
+        );
+    }
 }
